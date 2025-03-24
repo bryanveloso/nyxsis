269b36b1ff5aad556a744488848aa49873f82692
Bryan Veloso <bryan@velo.so>
2025-03-23 23:36:50 -0700

    Use Discord for auth.

--- src/auth.ts
+++ src/auth.ts
@@ -1,23 +1,6 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
+import NextAuth from 'next-auth'
+import Discord from 'next-auth/providers/discord'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Credentials({
-      credentials: {
-        email: {},
-        username: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        let user = null
-
-        if (!user) {
-          throw new Error("No user found")
-        }
-
-        return user
-      },
-    }),
-  ],
+  providers: [Discord]
 })
